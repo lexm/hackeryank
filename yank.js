b6e320bde95aeec4a5218bae3c006d604e2463fc
Lex Myers <lex.myers@gmail.com>
2017-06-03 20:03:13 -0700

    use single quotes

--- yank.js
+++ yank.js
@@ -1,13 +1,15 @@
-var outCode = "";
+var outCode = '';
 $(".CodeMirror-code div pre > span").each(function() {
-  outCode += this.textContent + "\n";
+  outCode += this.textContent + '\n';
 });
 var pathArray = [];
 $(".bcrumb span").each(function() {
   pathArray.push(this.textContent);
 });
+var ext = '';
 var lang1 = $(".select2-container span").text().split(' ')[0];
 var lang2 = $(".pull-left .msT").text().split(' ')[1];
 var lang = lang1 ? lang1 : lang2;
+if(lang === 'Python')
 var urlArray = window.location.pathname.split('/');
 var fName = urlArray[2];
