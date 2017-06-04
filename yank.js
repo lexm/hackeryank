1bdd4a0c7738dff9017f0b29366d423edabed59e
Lex Myers <lex.myers@gmail.com>
2017-06-03 22:55:37 -0700

    now downloads code

--- yank.js
+++ yank.js
@@ -1,7 +1,3 @@
-function downloadCode(code, fname) {
-  var link = '<a href="data:text/html,' + code + '" download="' + fname + '">'
-  $(".grey-header div:nth-child(5)").append(link);
-}
 
 var outCode = '';
 $(".CodeMirror-code div pre > span").each(function() {
@@ -11,12 +7,22 @@ var pathArray = [];
 $(".bcrumb span").each(function() {
   pathArray.push(this.textContent);
 });
+var urlArray = window.location.pathname.split('/');
+if(urlArray[3] === 'submissions') {
+  var lang = $(".pull-left .msT").text().split(' ')[1];
+  var spot = $(".submissions-details .pull-left p");
+
+} else {
+  var lang = $(".select2-container span").text().split(' ')[0];
+  var spot = $(".grey-header div:nth-child(5)");
+}
 var ext = '';
-var lang1 = $(".select2-container span").text().split(' ')[0];
-var lang2 = $(".pull-left .msT").text().split(' ')[1];
-var lang = lang1 ? lang1 : lang2;
 if(lang === 'Python' || lang === 'Pypy') {
   ext = '.py';
 }
-var urlArray = window.location.pathname.split('/');
 var filename = urlArray[2] + ext;
+var spotText = spot.text();
+spot.html("<a>" + spotText + "</a>");
+tag = spot.find("a");
+tag.attr("href", "data:text/plain;charset=UTF-8," + encodeURIComponent(outCode));
+tag.attr("download", filename);
