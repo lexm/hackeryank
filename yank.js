d386d8bc10c48c5794fc91befa7f408e612b3dee
Lex Myers <lex.myers@gmail.com>
2017-06-03 19:58:21 -0700

    trim version form lang1

--- yank.js
+++ yank.js
@@ -6,7 +6,7 @@ var pathArray = [];
 $(".bcrumb span").each(function() {
   pathArray.push(this.textContent);
 });
-var lang1 = $(".select2-container span").text();
+var lang1 = $(".select2-container span").text().split(' ')[0];
 var lang2 = $(".pull-left .msT").text().split(' ')[1];
 var lang = lang1 ? lang1 : lang2;
 var urlArray = window.location.pathname.split('/');
