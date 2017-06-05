e82d6d14f3696af8b0d3e9ef9cb70c9e2abf56d7
Lex Myers <lex.myers@gmail.com>
2017-06-05 13:59:02 -0700

    added checks for lower directory existence

--- sandbox/model.sh
+++ sandbox/model.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 HACKERRANK_REPO=~/Dev/hackerrank/hackerrank-code
+BCRUMB1=Algorithms
+BCRUMB2=Warmup
 if [ -d $HACKERRANK_REPO ]; then
   echo $HACKERRANK_REPO exists;
+fi
+if [ -d $HACKERRANK_REPO/$BCRUMB1 ]; then
+  echo $HACKERRANK_REPO/$BCRUMB1 exists;
+fi
+if [ -d $HACKERRANK_REPO/$BCRUMB1/$BCRUMB2 ]; then
+  echo $HACKERRANK_REPO/$BCRUMB1/$BCRUMB2 exists;
 fi
