2c2967f084a0ff08c8865853ebf870691a44208f
Lex Myers <lex.myers@gmail.com>
2017-06-05 14:10:31 -0700

    check if dir is repo

--- sandbox/model.sh
+++ sandbox/model.sh
@@ -2,12 +2,10 @@
 HACKERRANK_REPO=~/Dev/hackerrank/hackerrank-code
 BCRUMB1=Algorithms
 BCRUMB2=Warmup
-if [ -d $HACKERRANK_REPO ]; then
-  echo $HACKERRANK_REPO exists;
-fi
-if [ -d $HACKERRANK_REPO/$BCRUMB1 ]; then
-  echo $HACKERRANK_REPO/$BCRUMB1 exists;
-fi
-if [ -d $HACKERRANK_REPO/$BCRUMB1/$BCRUMB2 ]; then
-  echo $HACKERRANK_REPO/$BCRUMB1/$BCRUMB2 exists;
+if [ ! -d $HACKERRANK_REPO ]; then
+  echo $HACKERRANK_REPO does not exist;
+elif [ ! -d $HACKERRANK_REPO/.git ]; then
+  echo $HACKERRANK_REPO is not a git repo;
+else
+  mkdir -p $HACKERRANK_REPO/$BCRUMB1/$BCRUMB2
 fi
