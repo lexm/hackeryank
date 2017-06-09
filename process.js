d9506517c4f8510c71f4cb1ab54bdde400b1c1a6
Lex Myers <lex.myers@gmail.com>
2017-06-08 18:26:53 -0700

    move filePath to variable

--- process.js
+++ process.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const home = process.env.HOME;
+const filePath = '/Downloads/itertools-combinations.json';
 console.log(home);
 var scriptData;
-fs.readFile(home + '/Downloads/itertools-combinations.json', 'utf8', function(err, data) {
+fs.readFile(home + filePath, 'utf8', function(err, data) {
   if(err) throw err;
   scriptData = JSON.parse(data);
   console.log(scriptData);
