a46d19bb43fa5fce13157aab2d1da52d5bab0c53
Lex Myers <lex.myers@gmail.com>
2017-06-08 18:26:01 -0700

    show scriptData after loaded

--- process.js
+++ process.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const home = process.env.HOME;
+console.log(home);
 var scriptData;
-fs.readFile('~/Downloads/itertools-combinations.json', 'utf8', function(err, data) {
+fs.readFile(home + '/Downloads/itertools-combinations.json', 'utf8', function(err, data) {
   if(err) throw err;
   scriptData = JSON.parse(data);
+  console.log(scriptData);
 })
-console.log(scriptData);
