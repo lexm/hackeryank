44125dd8ca1c368fc532791822d128d520759f8c
Lex Myers <lex.myers@gmail.com>
2017-06-09 16:33:07 -0700

    get repo environment variable

--- process.js
+++ process.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const home = process.env.HOME;
+repo = process.env.HACKERRANK_REPO || home + '/hackerrank-code/';
 const filePath = '/Downloads/itertools-combinations.json';
-console.log(home);
+console.log(repo);
 var scriptData;
 fs.readFile(home + filePath, 'utf8', function(err, data) {
   if(err) throw err;
