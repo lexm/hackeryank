3f5dace6c62804f6f57aed69fa7a183f5e42e59d
Lex Myers <lex.myers@gmail.com>
2017-06-14 17:26:01 -0700

    removed zombie code

--- process.js
+++ process.js
@@ -1,11 +1,7 @@
-// const Promise = require('bluebird');
-// const readFile = Promise.promisify(require('fs').readFile);
 const fs = require('fs');
 const exec = require('child_process').exec;
 const home = process.env.HOME;
 const repo = process.env.HACKERRANK_REPO || home + '/hackerrank-code/';
-// const repo = home + '/nothing/';
-// let repo = home + '/';
 const filePath = process.argv[2] || home + '/Downloads/hr_download.json';
 console.log('filePath: ', filePath);
 
@@ -25,26 +21,14 @@ const makeCodeDir = function(repo, pathArray) {
 
 let scriptData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 let { pathArray, breadcrumb, message, filename, allCode} = scriptData;
-// console.log('pathArray: ', pathArray);
-// console.log('breadcrumb: ', breadcrumb);
-// console.log('message: ', message);
-// console.log('filename: ', filename);
-// console.log('allCode: ', allCode);
 let fullPath = makeCodeDir(repo, pathArray);
 fs.writeFile(fullPath + filename, allCode, function(err){
   if(err) throw err;
   console.log('File ' + filename + ' written');
-  var child1 = exec('cd ' + repo + breadcrumb + '/;git add ' + filename, function(error, stdout, stderr) {
-    console.log('stdout: ', stdout);
-    console.log('stderr: ', stderr);
+  exec('cd ' + repo + breadcrumb + '/;git add ' + filename, function(error) {
     if(error) console.error(error);
-    var child2 = exec('cd ' + repo + breadcrumb + '/;git commit -m "' + message + '"', function(error, stdout, stderr) {
-      console.log('stdout: ', stdout);
-      console.log('stderr: ', stderr);
+    exec('cd ' + repo + breadcrumb + '/;git commit -m "' + message + '"', function(error) {
       if(error) console.error(error);
     });
   });
 });
-// fs.writeFileSync(fullPath + filename, allCode);
-  // });
-// });
