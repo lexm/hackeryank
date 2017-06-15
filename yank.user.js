e651a530307db022bd383cc7ec94cef7c259fcf6
Lex Myers <lex.myers@gmail.com>
2017-06-14 17:46:53 -0700

    linted yank.user.js

--- yank.user.js
+++ yank.user.js
@@ -7,85 +7,84 @@
 // @author       Lex Myers
 // ==/UserScript==
 
-
 (function() {
-    $(document).ready(function() {
-        setTimeout(function() {
-          'use strict';
+  $(document).ready(function() {
+    setTimeout(function() {
+      'use strict';
 
-          var Solution = function(pathArray, filename) {
-            pathArray.shift();
-            this.progName = pathArray.pop();
-            this.pathArray = pathArray.map(function(cur) {
-              return cur.split(' ').join('_');
-            });
-            this.message = 'Solution to ' + pathArray.join(' > ') + ' > ' + this.progName;
-            this.filename = filename;
-            var outCode = [];
-            this.addCode = function(text) {
-              outCode.push(text);
-            };
-            this.allCode = function() {
-              return outCode.join('\n').replace(/[\u200B-\u200D\uFEFF]/g, '');
-            };
-          };
+      var Solution = function(pathArray, filename) {
+        pathArray.shift();
+        this.progName = pathArray.pop();
+        this.pathArray = pathArray.map(function(cur) {
+          return cur.split(' ').join('_');
+        });
+        this.message = 'Solution to ' + pathArray.join(' > ') + ' > ' + this.progName;
+        this.filename = filename;
+        var outCode = [];
+        this.addCode = function(text) {
+          outCode.push(text);
+        };
+        this.allCode = function() {
+          return outCode.join('\n').replace(/[\u200B-\u200D\uFEFF]/g, '');
+        };
+      };
 
-          Solution.prototype.genJSON = function() {
-            var result = {};
-            result.progName = this.progName;
-            result.pathArray = this.pathArray;
-            result.message = this.message;
-            result.filename = this.filename;
-            result.allCode = this.allCode();
-            return JSON.stringify(result);
-          };
+      Solution.prototype.genJSON = function() {
+        var result = {};
+        result.progName = this.progName;
+        result.pathArray = this.pathArray;
+        result.message = this.message;
+        result.filename = this.filename;
+        result.allCode = this.allCode();
+        return JSON.stringify(result);
+      };
 
-          var scrapePath = function() {
-            var pathArray = [];
-            $(".bcrumb span").each(function() {
-              pathArray.push(this.textContent);
-            });
-            return pathArray;
-          };
+      var scrapePath = function() {
+        var pathArray = [];
+        $('.bcrumb span').each(function() {
+          pathArray.push(this.textContent);
+        });
+        return pathArray;
+      };
 
-          var genFilename = function(is_json) {
-            var urlArray = window.location.pathname.split('/');
-            var lang = $(".pull-left .msT").text().replace(/^\s+|\s+$/g,'').split(' ')[1];
-            var ext = '';
-            if(lang === 'Python' || lang === 'Pypy') {
-              ext = '.py';
-            } else if(lang === 'JavaScript') {
-              ext = '.js';
-            } else if(lang === 'BASH') {
-              ext = '.sh';
-            } else if(lang === 'MySQL') {
-              ext = '.sql';
-            }
-            if(is_json) {
-              return urlArray[2] + '.json';
-            } else {
-              return urlArray[2] + ext;
-            }
-          };
+      var genFilename = function(is_json) {
+        var urlArray = window.location.pathname.split('/');
+        var lang = $('.pull-left .msT').text().replace(/^\s+|\s+$/g,'').split(' ')[1];
+        var ext = '';
+        if(lang === 'Python' || lang === 'Pypy') {
+          ext = '.py';
+        } else if(lang === 'JavaScript') {
+          ext = '.js';
+        } else if(lang === 'BASH') {
+          ext = '.sh';
+        } else if(lang === 'MySQL') {
+          ext = '.sql';
+        }
+        if(is_json) {
+          return urlArray[2] + '.json';
+        } else {
+          return urlArray[2] + ext;
+        }
+      };
 
-          var genSolution = function(filename) {
-            var solution = new Solution(scrapePath(), genFilename());
-            $(".CodeMirror-code div pre > span").each(function() {
-              solution.addCode(this.textContent);
-            });
-            return solution;
-          };
+      var genSolution = function() {
+        var solution = new Solution(scrapePath(), genFilename());
+        $('.CodeMirror-code div pre > span').each(function() {
+          solution.addCode(this.textContent);
+        });
+        return solution;
+      };
 
-          var addLinkToPage = function() {
-            var spot = $(".submissions-details .pull-left p");
-            var spotText = spot.text();
-            spot.html("<a>" + spotText + "</a>");
-            var tag = spot.find("a");
-            tag.attr("href", "data:text/plain;charset=UTF-8," + encodeURIComponent(genSolution().genJSON()));
-            tag.attr("download", "hr_download.json");
-          };
+      var addLinkToPage = function() {
+        var spot = $('.submissions-details .pull-left p');
+        var spotText = spot.text();
+        spot.html('<a>' + spotText + '</a>');
+        var tag = spot.find('a');
+        tag.attr('href', 'data:text/plain;charset=UTF-8,' + encodeURIComponent(genSolution().genJSON()));
+        tag.attr('download', 'hr_download.json');
+      };
 
-          addLinkToPage();
-        }, 3000);
-    });
+      addLinkToPage();
+    }, 3000);
+  });
 })();
